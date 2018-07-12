28dcd5137d79261445ac8a38f17ab99474dcbd88
CC Wang <jlps6716@hotmail.com>
2018-07-13 06:52:56 +0800

    add filter of good

--- read.py
+++ read.py
@@ -13,13 +13,18 @@ print('檔案讀取完了，總共有', len(data),'筆資料')
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-print('留言平均長度為',sum_len/len(data))
+print('留言平均長度為',sum_len/len(data),'字')
 
-#留言篩選
+#留言篩選(len<100)
 new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print('一共有', len(new),'筆留言長度小於100')
-print(new[0])
-print(new[1])
+print('一共有', len(new),'筆留言長度小於100字')
+
+#留言篩選(good)
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)	
+print('一共有', len(good),'筆留言有good')
