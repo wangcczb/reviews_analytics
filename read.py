c900115db3937baf6f533b35b6a758b8d28d9a0b
CC Wang <jlps6716@hotmail.com>
2018-07-13 06:43:42 +0800

    add filter

--- read.py
+++ read.py
@@ -1,3 +1,4 @@
+#讀取資料
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
@@ -8,8 +9,17 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data),'筆資料')
 
+#計算留言平均長度
 sum_len = 0
 for d in data:
-	len(d)
 	sum_len += len(d)
 print('留言平均長度為',sum_len/len(data))
+
+#留言篩選
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new),'筆留言長度小於100')
+print(new[0])
+print(new[1])
